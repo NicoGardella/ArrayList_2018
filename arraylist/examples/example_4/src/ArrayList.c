205f4b0e452bd24fa002b30efc4d337809a7d6a7
alumno <alumno@lab6pc0>
2018-06-06 22:02:24 -0300

    commit con punto 6 funcionando (al_contains)

--- arraylist/examples/example_4/src/ArrayList.c
+++ arraylist/examples/example_4/src/ArrayList.c
@@ -157,6 +157,19 @@ void* al_get(ArrayList* this, int index)
 int al_contains(ArrayList* this, void* pElement)
 {
     int returnAux = -1;
+    int i = 0;
+    if(this!=NULL && pElement!=NULL)
+    {
+        returnAux = 0;
+        for(i==0;i<this->size;i++)
+        {
+            if(pElement==this->pElements[i])
+            {
+                returnAux=1;
+                break;
+            }
+        }
+    }
 
     return returnAux;
 }
